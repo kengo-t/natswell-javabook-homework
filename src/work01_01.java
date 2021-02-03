dc8b42c8bbcbd5cbe021d610b4c34d60305b8dc7
kengo-t <kengo-t@natswell.com>
2021-02-03 12:07:12 +0900

    submit work1-1

---
+++ src/work01_01.java
@@ -0,0 +1,10 @@
+
+public class work01_01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		double height=1.65,weight=55;
+		System.out.print(weight/(height*height));
+	}
+
+}
