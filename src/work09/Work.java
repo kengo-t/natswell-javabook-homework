7635eaf671de1ab589d291a31c097e89a5b180c1
kengo-t <kengo-t@natswell.com>
2021-02-15 18:38:10 +0900

    submit work09

--- src/work09/Work.java
+++ src/work09/Work.java
@@ -1,8 +1,15 @@
 package work09;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 9章
@@ -16,27 +23,36 @@ public class Work {
 
 	public static void main(String[] args) {
 		
+		
+		
 		/*
-		 * 現在の日付から、入力された日数後の日付を成形して表示する
+		 * 
+		 * 日付データのファイルを100日後の日付に変更したファイルを生成する
 		 * 
 		 */
 		
-		Scanner sc = new Scanner(System.in);
-		int d=-1;
-		while(d<=0){
-			System.out.println("正の整数を入力してください");
-			try {
-				d = Integer.parseInt(sc.next());			
-			} catch (Exception e) {
-				System.out.println("不正な入力値です");
-				continue;
-			}
+		Path from = Paths.get("./src/work09/date.txt");
+		List<String> strings=null;
+		try(BufferedReader br = Files.newBufferedReader(from)){
+			strings = br.lines().collect(Collectors.toList());
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		
-		String date = DateTimeFormatter.ofPattern("y年M月d日")
-										.format(LocalDate.now().plusDays(d));
 		
-		System.out.println(d+"日後は"+date+"です");
+		Path to = Paths.get("./src/work09/plus100Date.txt");
+		//LocalDateクラスに変換するためのフォーマットを作成
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("y-M-d");
+		try(BufferedWriter bw = Files.newBufferedWriter(to)){
+			for (String str : strings) {
+				//フォーマットに従ってLocalDateクラスのインスタンスに変換したのち100日後に変更
+				LocalDate temp = LocalDate.from(format.parse(str)).plusDays(100);
+				//元のフォーマットに戻し出力ファイルに書き込む
+				bw.append(format.format(temp)+"\n");
+			}			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
